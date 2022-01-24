0a2e6463e69b1d9e7bc2c7553a8f11399c3f274e
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-01-24 15:55:54 +0600

    Arrays

--- Main.java
+++ Main.java
@@ -1,15 +1,16 @@
 package com.company;
 
-import java.awt.*;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        // System.out.println("Hello World");
-        Point point1 = new Point(1,1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+        int[] numbers = new int[5];
+        numbers[0] = 10;
+        numbers[1] = 55;
+        // String Rep of Arrays
+        String msg = Arrays.toString(numbers);
+        System.out.println(msg);
     }
 }
