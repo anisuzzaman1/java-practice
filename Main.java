50e6c8a43307ccfbf64ddba683e2f471ed00927e
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-01-24 15:23:01 +0600

    Java file

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // System.out.println("Hello World");
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
+    }
+}
