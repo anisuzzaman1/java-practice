03172d804d8cdf1e01f22079496a7ccf6e14ce43
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-01-28 01:53:04 +0600

    Simple Mortgage Caclulator Project

--- Main.java
+++ Main.java
@@ -1,53 +1,33 @@
 package com.company;
-
-import kotlin.text.UStringsKt;
-
-import java.util.Arrays;
+import java.text.NumberFormat;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-//        // write your code here
-//        // String Rep of Arrays Old Method
-//        int[] numbers = new int[5];
-//        numbers[0] = 10;
-//        numbers[1] = 55;
-//        String msg = Arrays.toString(numbers);
-//        //System.out.println(msg);
-//
-//        // String Rep of Arrays New Method
-//        int[] num2 = {2,9,4,3,1,7,15,9};
-//        Arrays.sort(num2);
-//        // System.out.println(Arrays.toString(num2));
-//
-//        // Multi Dimentional Array //
-//        int[][] num3 = new int[2][3]; // 2rows x 3column
-//        num3[1][0] = 777;
-//        // Printing Multi Dim Arrary
-//        // System.out.println(Arrays.deepToString(num3));
-//
-//        int[][] num4 = {{1,2,3},{4,5,6},{7,8,9}};
-//        System.out.println(Arrays.deepToString(num4));
-        // arr = new arrary();
 
-        // Conversion of datatype
-        String number="3";
-        int num2 = Integer.parseInt(number);
-        int numValue = num2 +2;
-        System.out.println(numValue);
-    }
+    Scanner inputText1 = new Scanner(System.in);
+    System.out.print("Principle: ");
+    double principal = inputText1.nextDouble();
 
-    static int a1(int[] a)
-    {
-        if (a == null || a.length % 2 == 0)
-            return 0;
-        int midIndex = a.length / 2 ;
-        int middleItem = a[midIndex];
-        for (int i=0; i<a.length; i++)
-        {
-            if (i != midIndex && middleItem >= a[i])
-                return 0;
-        }
-        return 1;
+    Scanner inputText2 = new Scanner(System.in);
+    System.out.print("Annual Interest Rate: ");
+    float annualInterestRate = inputText2.nextFloat();
+
+    Scanner inputText3 = new Scanner(System.in);
+    System.out.print("Period (Years): ");
+    int years = inputText3.nextInt();
+
+    float monthlyInterest = (annualInterestRate /100)/12;
+    int numberOfPayment = years * 12;
+
+    double upperCalc = monthlyInterest * (Math.pow((1+monthlyInterest), numberOfPayment));
+    double lowerCalc = Math.pow((1+monthlyInterest),numberOfPayment)-1;
+    double monthlyPayment = (principal * upperCalc)/lowerCalc;
+
+    //String finalMortgage = "1234567";
+    NumberFormat currency = NumberFormat.getCurrencyInstance();
+    String result = currency.format(monthlyPayment);
+    System.out.println("Mortgage: "+result);
+
     }
 }
