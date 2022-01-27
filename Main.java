e8ba8f10b980487f288f9b88af403fbf435604be
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-01-28 02:19:49 +0600

    Simple Mortgage Caclulator Project

--- Main.java
+++ Main.java
@@ -1,33 +1,34 @@
 package com.company;
+
 import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-    Scanner inputText1 = new Scanner(System.in);
-    System.out.print("Principle: ");
-    double principal = inputText1.nextDouble();
+        Scanner inputText1 = new Scanner(System.in);
+        System.out.print("Principle: ");
+        double principal = inputText1.nextDouble();
 
-    Scanner inputText2 = new Scanner(System.in);
-    System.out.print("Annual Interest Rate: ");
-    float annualInterestRate = inputText2.nextFloat();
+        Scanner inputText2 = new Scanner(System.in);
+        System.out.print("Annual Interest Rate: ");
+        float annualInterestRate = inputText2.nextFloat();
 
-    Scanner inputText3 = new Scanner(System.in);
-    System.out.print("Period (Years): ");
-    int years = inputText3.nextInt();
+        Scanner inputText3 = new Scanner(System.in);
+        System.out.print("Period (Years): ");
+        int years = inputText3.nextInt();
 
-    float monthlyInterest = (annualInterestRate /100)/12;
-    int numberOfPayment = years * 12;
+        float monthlyInterest = (annualInterestRate / 100) / 12;
+        int numberOfPayment = years * 12;
 
-    double upperCalc = monthlyInterest * (Math.pow((1+monthlyInterest), numberOfPayment));
-    double lowerCalc = Math.pow((1+monthlyInterest),numberOfPayment)-1;
-    double monthlyPayment = (principal * upperCalc)/lowerCalc;
+        double upperCalc = monthlyInterest * (Math.pow((1 + monthlyInterest), numberOfPayment));
+        double lowerCalc = Math.pow((1 + monthlyInterest), numberOfPayment) - 1;
+        double monthlyPayment = (principal * upperCalc) / lowerCalc;
 
-    //String finalMortgage = "1234567";
-    NumberFormat currency = NumberFormat.getCurrencyInstance();
-    String result = currency.format(monthlyPayment);
-    System.out.println("Mortgage: "+result);
+        //String finalMortgage = "1234567";
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String result = currency.format(monthlyPayment);
+        System.out.println("Mortgage: " + result);
 
     }
 }
--- Mortgage.java
+++ Mortgage.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import java.text.NumberFormat;
 import java.util.Scanner;
 
@@ -17,17 +18,17 @@ public class Mortgage {
         System.out.print("Period (Years): ");
         int years = inputText3.nextInt();
 
-        float monthlyInterest = (annualInterestRate /100)/12;
+        float monthlyInterest = (annualInterestRate / 100) / 12;
         int numberOfPayment = years * 12;
 
-        double upperCalc = monthlyInterest * (Math.pow((1+monthlyInterest), numberOfPayment));
-        double lowerCalc = Math.pow((1+monthlyInterest),numberOfPayment)-1;
-        double monthlyPayment = (principal * upperCalc)/lowerCalc;
+        double upperCalc = monthlyInterest * (Math.pow((1 + monthlyInterest), numberOfPayment));
+        double lowerCalc = Math.pow((1 + monthlyInterest), numberOfPayment) - 1;
+        double monthlyPayment = (principal * upperCalc) / lowerCalc;
 
         //String finalMortgage = "1234567";
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         String result = currency.format(monthlyPayment);
-        System.out.println("Mortgage: "+result);
+        System.out.println("Mortgage: " + result);
 
     }
 }
