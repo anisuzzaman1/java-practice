8704cc795433acc3126a20091cd23890f3152fbb
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-01-29 02:58:56 +0600

    Updated

--- Main.java
+++ Main.java
@@ -6,20 +6,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner inputText1 = new Scanner(System.in);
+        // Declaration of variable
+        final byte MONTH_IN_YEAR = 12;
+        final byte PERCENTAGE = 100;
+
+        Scanner input = new Scanner(System.in);
+
         System.out.print("Principle: ");
-        double principal = inputText1.nextDouble();
+        double principal = input.nextDouble();
 
-        Scanner inputText2 = new Scanner(System.in);
         System.out.print("Annual Interest Rate: ");
-        float annualInterestRate = inputText2.nextFloat();
+        float annualInterestRate = input.nextFloat();
 
-        Scanner inputText3 = new Scanner(System.in);
         System.out.print("Period (Years): ");
-        int years = inputText3.nextInt();
+        int years = input.nextInt();
 
-        float monthlyInterest = (annualInterestRate / 100) / 12;
-        int numberOfPayment = years * 12;
+        float monthlyInterest = (annualInterestRate / PERCENTAGE) / MONTH_IN_YEAR;
+        int numberOfPayment = years * MONTH_IN_YEAR;
 
         double upperCalc = monthlyInterest * (Math.pow((1 + monthlyInterest), numberOfPayment));
         double lowerCalc = Math.pow((1 + monthlyInterest), numberOfPayment) - 1;
