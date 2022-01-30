37eb3b7ff73388d6576577f1badd8e5fb0130f4f
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-01-31 00:53:34 +0600

    Ternary.java

--- Ternary.java
+++ Ternary.java
@@ -1,16 +1,12 @@
 package com.company;
 
-import java.text.NumberFormat;
-import java.util.Scanner;
 
-public class Main {
+// Ternery Operator in Java
+public class Ternary {
     public static void main(String[] args) {
-
         int income = 120_000;
 //        String className = "Economy";
         String className = income > 100_000 ? "First" : "Economy";
         System.out.println(className);
-
-
     }
 }
--- Weather.java
+++ Weather.java
@@ -5,14 +5,9 @@ import java.util.Scanner;
 
 public class Weather {
     public static void main(String[] args) {
-        int inputTemp = 15;
-        if (inputTemp > 30) {
-            System.out.println("Its a HOT day, Please Drink some water!!!");
-        } else if (inputTemp <= 30 && inputTemp >= 20) {
-            System.out.println("Its a nice day, enjoy!!!");
-        } else {
-            System.out.println("Its a cold day, stay at home !!!");
-        }
+        int income = 100_000;
+        // The Condition shows True or false
+        boolean taxable = (income > 120_000);
 
     }
 }
