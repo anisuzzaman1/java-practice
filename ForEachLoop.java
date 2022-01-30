66beca4f275b5da3b25f0f4c60e9ddb1ab771bc6
anisuzzaman1 <anisuzzaman2112@gmail.com>
2022-01-31 01:38:00 +0600

    ForEachLoop.java

--- ForEachLoop.java
+++ ForEachLoop.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class ForEachLoop {
     public static void main(String[] args) {
         String[] fruits = {"Mango", "Apple", "Orange", "Jackfruit", "Dragon Fruits"};
 
